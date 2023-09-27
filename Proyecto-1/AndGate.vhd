e062d01915a29602bbe9331bfe03d8d26b192a51
Farid c <143665202+farid3312@users.noreply.github.com>
2023-09-27 12:46:06 -0500

    Add files via upload

--- Proyecto-1/AndGate.vhd
+++ Proyecto-1/AndGate.vhd
@@ -1,1 +1,23 @@
+-- Librerias y paquetes
+library IEEE;
+use IEEE.std_logic_1164.all;
 
+--Entidad
+entity AndGate is
+       port(
+		 x : in std_logic;
+		 y : in std_logic;
+		 f : out std_logic
+		 );
+end entity;
+
+--Arquitectura
+
+architecture AndGate_arch of AndGate is
+signal s : std_logic;
+
+begin
+       s <= x nand y;
+		 f <= not s;
+
+end architecture;
