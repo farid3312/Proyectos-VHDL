01f721ad3b6847630a6bfa4af1665561c27acbca
Farid c <143665202+farid3312@users.noreply.github.com>
2023-09-27 12:40:01 -0500

    Add files via upload

--- Proyecto-1/Compuerta_or.vhd
+++ Proyecto-1/Compuerta_or.vhd
@@ -1,1 +1,23 @@
+--Prueba OR using compuertas NAND
+-- librerias
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+--definicion de entidades 
+
+entity Compuerta_or is
+    Port ( x, y : in  std_logic;
+           f    : out std_logic);
+end Compuerta_or;
+
+-- definicion de la arquitectura
+
+architecture Behavioral of compuerta_or is
+    signal NAND_x  : std_logic;
+    signal NAND_y  : std_logic;
+begin
+    NAND_x <= x nand x;
+    NAND_y <= y nand y;
+	 f <= NAND_x nand NAND_y;
+end Behavioral;
 
