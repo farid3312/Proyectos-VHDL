461905adb69aa056086dda1cb953d1497ea8877f
Farid c <143665202+farid3312@users.noreply.github.com>
2023-09-27 13:19:12 -0500

    Add files via upload

--- Proyecto-1/DMux4_way.vhd
+++ Proyecto-1/DMux4_way.vhd
@@ -1,1 +1,42 @@
+library ieee;
+use ieee.std_logic_1164.all;
 
+entity DMux4_way is
+    port (
+        w : in std_logic;
+        sel0 : in std_logic;
+        sel1 : in std_logic;
+        f1 : out std_logic;
+        f2 : out std_logic;
+        f3 : out std_logic;
+        f4 : out std_logic
+    );
+end entity;
+
+architecture arch_DMux4_way of DMux4_way is
+begin
+    process (w, sel0, sel1)
+    begin
+        if (sel0 = '0' and sel1 = '0') then
+            f1 <= w;
+            f2 <= '0';
+            f3 <= '0';
+            f4 <= '0';
+        elsif (sel0 = '0' and sel1 = '1') then
+            f1 <= '0';
+            f2 <= w;
+            f3 <= '0';
+            f4 <= '0';
+        elsif (sel0 = '1' and sel0 = '0') then
+            f1 <= '0';
+            f2 <= '0';
+            f3 <= w;
+            f4 <= '0';
+        elsif (sel1 = '1' and sel0 = '1') then
+            f1 <= '0';
+            f2 <= '0';
+            f3 <= '0';
+            f4 <= w;
+        end if;
+    end process;
+end architecture;
