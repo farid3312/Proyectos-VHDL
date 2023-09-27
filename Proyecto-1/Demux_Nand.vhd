47d8d450c3eab5b8df17e35f08024458d27ec961
Farid c <143665202+farid3312@users.noreply.github.com>
2023-09-27 12:57:47 -0500

    Add files via upload

--- Proyecto-1/Demux_Nand.vhd
+++ Proyecto-1/Demux_Nand.vhd
@@ -1,1 +1,18 @@
+--Prueba demux using compuertas NAND
+-- librerias
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
 
+--definicion de entidades 
+entity Demux_Nand is 
+  Port ( x,sel : in  STD_LOGIC;
+         f,g   : out STD_LOGIC);
+end Demux_Nand;
+
+-- definicion de la arquitectura
+
+architecture Behavioral of Demux_Nand is
+begin 
+  f <= (not sel) and x;
+  g <= sel and x;
+end Behavioral;
