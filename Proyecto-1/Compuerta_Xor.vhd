5ec373013c17824f0e46199f6b99a5ddb75922b8
Farid c <143665202+farid3312@users.noreply.github.com>
2023-09-27 12:52:02 -0500

    Add files via upload

--- Proyecto-1/Compuerta_Xor.vhd
+++ Proyecto-1/Compuerta_Xor.vhd
@@ -1,1 +1,23 @@
+--Prueba OR using compuertas NAND
+-- librerias
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
 
+--definicion de entidades 
+
+entity Compuerta_Xor is
+    Port ( x, y : in  STD_LOGIC;
+           f    : out STD_LOGIC);
+end Compuerta_Xor;
+
+-- definicion de la arquitectura
+
+architecture Behavioral of compuerta_Xor is
+    signal n0 : STD_LOGIC;
+	 signal n1 : STD_LOGIC;
+	 
+begin
+   n0 <= x and (y nand y);
+	n1 <= (x nand x) and y;
+	f <= n0 or n1;
+end Behavioral;
