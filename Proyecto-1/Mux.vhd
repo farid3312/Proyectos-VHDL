632c7da58576171d1371152113304d7aef0c1dd9
Farid c <143665202+farid3312@users.noreply.github.com>
2023-09-27 12:54:58 -0500

    Add files via upload

--- Proyecto-1/Mux.vhd
+++ Proyecto-1/Mux.vhd
@@ -1,1 +1,22 @@
+library	IEEE;
+use IEEE.std_logic_1164.all;
 
+-- Entity (Interface)
+entity Mux is
+	port(
+		x	:	in 	std_logic;
+		y	:	in		std_logic;
+		sel:	in 	std_logic;
+		f	:	out	std_logic
+	);
+end entity;
+
+-- Architecture (Implementation)
+architecture arch of Mux is
+signal x1, x2 : std_logic;
+
+begin
+	x1 <= ( not sel ) and x;
+	x2 <= sel and y;
+	f <= x1 or x2;
+end architecture;
