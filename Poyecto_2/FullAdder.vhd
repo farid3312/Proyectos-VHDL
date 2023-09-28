123ab7d386ae0fd4d9f2bd037121189cc6878247
Farid c <143665202+farid3312@users.noreply.github.com>
2023-09-27 20:40:13 -0500

    Add files via upload

--- Poyecto_2/FullAdder.vhd
+++ Poyecto_2/FullAdder.vhd
@@ -1,1 +1,52 @@
+--Sumador Completa
+-- File Name: FullAdder.vhdl
 
+-- Library and packages
+library ieee;
+use ieee.std_logic_1164.all;
+
+--Suma 3 entradas ;
+
+entity fullAdder is
+	Port(
+		x 	: in std_logic;
+		y 	: in std_logic;
+		z  : in std_logic;	
+		sum 	: out std_logic;
+		carry : out std_logic
+	);
+end entity;
+
+architecture arch of fullAdder is
+	signal sum1: std_logic;
+	signal carry1: std_logic;
+	signal carry2: std_logic;
+	
+	component halfAdder is
+		Port(
+			x	:	in 	std_logic;
+			y	:	in		std_logic;
+			sum 	: out std_logic;
+			carry : out std_logic
+		);
+	end component;
+begin
+	halfAdder1: halfAdder
+		Port map(
+			x => x,
+			y => y,
+			sum => sum1,
+			carry => carry1
+		);
+	halfAdder2: halfAdder
+		Port map(
+			x => sum1,
+			y => z,
+			sum => sum,
+			carry => carry2
+		);
+	
+		carry <= carry1 or carry2;
+		
+
+end architecture;
