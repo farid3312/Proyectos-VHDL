d6e85f4778f2dde504db5221ed3c0aaad6d8cb8a
Farid c <143665202+farid3312@users.noreply.github.com>
2023-09-27 20:38:30 -0500

    Add files via upload

--- Poyecto_2/halfAdder.vhd
+++ Poyecto_2/halfAdder.vhd
@@ -1,1 +1,26 @@
+--Semisumador 
+-- File Name: HalfAdder.vhdl
+
+--lIBRERÍAS Y PAQUETES
+library ieee;
+use ieee.std_logic_1164.all;
+
+--ENTIDAD
+entity halfAdder is 
+	port(
+	x,y: in std_logic;
+	sum, carry: out std_logic
+	
+	);
+end entity;
+
+
+--ARQUITECTURA 
+
+architecture arch of halfAdder is
+begin
+	 sum <= x xor y;
+	 carry <= x and y;
+	 	 
+end architecture;
 
